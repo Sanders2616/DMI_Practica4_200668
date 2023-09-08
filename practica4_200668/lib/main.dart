ef0b0b51d3feddd2376c6c1dc441fedcb51267ae
Sanders2616 <sandra.aguilarsantos@utxicotepec.edu.mx>
2023-09-08 14:06:59 -0600

    color

--- practica4_200668/lib/main.dart
+++ practica4_200668/lib/main.dart
@@ -2,3 +2,31 @@
 //Asignatura: Desarrollo Movil Integral
 //Grado:10 Grupo:A
 //Docente: Marco A. Ramirez Hernandez
+import 'package:flutter/material.dart';
+void main(){
+var app = MaterialApp(
+  debugShowCheckedModeBanner:false,
+  home:Scaffold(
+    appBar: AppBar(
+      title:Text("Sandra 200668 <3"),
+      backgroundColor: Color.fromARGB(128,128, 0, 128),
+    ),//Appbar
+    body:Container(
+      child:Center(
+        child:Text("Hola Mundo",
+        style: TextStyle(fontSize:20.0),
+        ),//Text
+      ),//Center
+    ),//Container
+
+  floatingActionButton:FloatingActionButton(
+    child: Icon(Icons.favorite,color: Colors.pink),
+    onPressed:(){print('click');},
+    ),//floatingActionButton
+
+  )//Scaffold
+);  //MaterialApp
+
+    runApp(app);
+
+}
